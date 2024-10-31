ac855e626eb3422483fecf5a128fef2c4a7e8e34
2023202060003 <2023202060003@JP-LAB04-15>
2024-10-30 22:01:40 -0400

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿
+VeiculoDAO vdao = new VeiculoDAO();
+do
+{
+    Console.WriteLine("Digite 1 para cadastrar um novo veículo | 2 para listar todos | 3 para deletar um veículo | 4 para consultar | 5 para sair");
+    int opcao = int.Parse(Console.ReadLine());
+
+    if (opcao == 1)
+    {
+        Veiculo v = new Veiculo();
+        Console.WriteLine("Informe a placa");
+        v.placa = Console.ReadLine();
+        Console.WriteLine("Informe o modelo");
+        v.modelo = Console.ReadLine();
+
+        vdao.Cadastrar(v);
+    }
+    else if (opcao == 2)
+    {
+        List<Veiculo> veiculos = vdao.ListarTodos();
+        foreach (Veiculo v in veiculos)
+        {
+            Console.WriteLine(v.modelo + " " + v.placa);
+        }
+    }
+    else if (opcao == 3)
+    {
+        Console.WriteLine("Digite a placa do veículo a ser deletado");
+        string placa = Console.ReadLine();
+      
+        vdao.Deletar(placa);      
+    }
+    else if (opcao == 4)
+    {
+        Console.WriteLine("Digite a placa do veícula que deseja consultar");       
+        string placa = Console.ReadLine();
+        Veiculo veiculo = vdao.Consultar(placa);
+        Console.WriteLine(veiculo.modelo + " " + veiculo.placa);
+    }
+    else
+    {
+        break;
+    }
+
+} while (true);
+
+
---
+++ Veiculo.cs
@@ -0,0 +1,46 @@
+﻿public class Veiculo
+{
+    public string placa;
+    public string numeMotor;
+    public string niv;
+    public string renavan;
+    public string modelo;
+    public string marca;
+
+    public Veiculo(string placa, string numeMotor, string niv, string renavan, string modelo, string marca)
+    {
+        this.placa = placa;
+        this.numeMotor = numeMotor;
+        this.niv = niv;
+        this.renavan = renavan;
+        this.modelo = modelo;
+        this.marca = marca;
+    }
+}
+
+public class VeiculoDAO
+{
+    public List<Veiculo> listaVeiculos = new List<Veiculo>();
+
+    public void Cadastrar(Veiculo v)
+    {
+        listaVeiculos.Add(v);
+    }
+
+    public List<Veiculo> ListarTodos() 
+    { 
+        return listaVeiculos; 
+    }
+
+    public void Deletar(string placa)
+    {
+       Veiculo vExcluir = listaVeiculos.First(p => p.placa == placa);
+        listaVeiculos.Remove(vExcluir);
+    }
+
+    public Veiculo Consultar(string placa)
+    {
+        Veiculo veiculo = listaVeiculos.First(p => p.placa == placa);
+        return veiculo;  
+    }
+}
